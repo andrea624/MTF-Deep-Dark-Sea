c5543117d16421492503fe87674ec26475364396
Andrea Morales Villegas <moralesa7412@gmail.com>
2025-03-04 12:24:20 -0800

    created main.js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Names: Andrea Martinez, Andrea Morales Villegas
+// Game Title: Deep Dark Sea
+// Time Completion
+
+let config = {
+    type: Phaser.AUTO,
+    height: 640,
+    width: 960,
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+}
