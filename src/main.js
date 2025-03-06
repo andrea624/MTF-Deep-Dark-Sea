9bfc736b0ae139200536d1c9c7cf99c75643cfbc
Andrea Morales Villegas <moralesa7412@gmail.com>
2025-03-05 17:00:50 -0800

    worked on menu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // Names: Andrea Martinez, Andrea Morales Villegas
 // Game Title: Deep Dark Sea
-// Time Completion
+// Time Completion: 
+// log: 3/5: 4 hours 
 
 let config = {
     type: Phaser.AUTO,
@@ -8,6 +9,8 @@ let config = {
     width: 960,
     scale: {
         autoCenter: Phaser.Scale.CENTER_BOTH
-    }
+    },
+    scene: [ Menu ]
 }
+
 let game = new Phaser.Game(config)
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,1 +1,20 @@
-// hello
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene")
+    }
+
+    preload() {
+        // load background
+        this.preload.image('titlescreen', './assets/images/menuscreen.png')
+    }
+
+    create() {
+        // add menu background image
+        this.add.image(0, 0, 'titlescreen').setOrigin(0, 0).setDisplaySize(game.config.width, game.config.height);
+
+    }
+
+    update() {
+
+    }
+}
