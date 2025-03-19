90b3809b217d560e03ec5afa145f82567acf7f97
andrea624 <amart434@ucsc.edu>
2025-03-19 00:07:23 -0700

    added credits to scene

--- src/scenes/Win.js
+++ src/scenes/Win.js
@@ -8,6 +8,11 @@ class Win extends Phaser.Scene {
             fontSize: '64px',
             fill: '#FFF'
         }).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY + 120, 'comixloud', 'Press Enter to go to Menu', 30).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY + 170, 'comixloud', 'Credits:', 20).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY + 200, 'comixloud', 'Sounds: underwater sound from freesound.org', 16).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY + 230, 'comixloud', 'Animations: Andrea Martinez', 16).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY + 260, 'comixloud', 'Artwork: Andrea Andrea Morales Villegas', 16).setOrigin(0.5)
 
         //adds key
         keyEnter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
